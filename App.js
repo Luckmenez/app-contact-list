100fdfa3b42656ab008d00cb0d03ad0f4f63cfe4
Lucas Menezes <luckmenez@gmail.com>
2020-09-10 23:45:52 -0300

    finalização do exercicio

--- App.js
+++ App.js
@@ -1,56 +1,37 @@
 import { StatusBar, setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import ContactItem from './components/contactItem';
+import ContactInput from './components/ContactInput';
 
 export default function App() {
-  const [contacts, setContacts] = useState([]);
+
   const [counter, setCounter] = useState(10);
+  const [contacts, setContacts] = useState([]);
 
-  const [number, setNumber] = useState('');
-  const getNumber = number => {setNumber(number)}
-
-  const [name, setName] = useState('');
-  const getName = name => {setName(name)};
-
-
-
-  const addContact = () => {
+  const addContact = (contact) => {
     setContacts(contacts => {
       setCounter(counter + 2);
-      return [...contacts, {key: counter.toString(), value: {name, number}}]
+      return [...contacts, {key: counter.toString(), value: contact}]
+    })
+  };
+
+
+  const removeContact = (key) => {
+    setContacts((contacts) => {
+      return contacts.filter(contact => contact.key != key )
     })
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputView}>
-
-        <TextInput style={styles.input} 
-          placeholder="Digite o nome"
-          value={name}
-          onChangeText={getName}
-        />
-
-        <View>
-          <TextInput style={styles.input}
-            value={number}
-            placeholder="Digite o número"
-            onChangeText={getNumber}
-          />        
-        </View>
-
-        <Button
-          title="Enviar"
-          onPress={addContact}
-        />
-      </View>
+      <ContactInput onAddContact={addContact}/>
       <FlatList 
         data={contacts}
         renderItem={
           (contact) => (
             <View style={styles.contactsList}>
-              <Text>{contact.item.value.name}</Text>
-              <Text>{contact.item.value.number}</Text>
+              <ContactItem onDeleteContact={removeContact} contact={contact.item.value} chave={contact.item.key}/>
             </View>
           )
         }
@@ -62,23 +43,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 50
-  },
-  inputView: {
-    marginBottom: 8
-  },
-  input:{
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
-    marginBottom: 4,
-    padding: 12,
-    textAlign: 'center',
-  },
-  contactsList: {
-    padding: 12,
-    backgroundColor: '#DDD',
-    borderColor: 'black',
-    borderWidth: 1,
-    marginBottom: 8,
-    borderRadius: 8
-  },
+  }
 });
